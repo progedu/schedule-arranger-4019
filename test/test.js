50205e0972e1e1a2b2d665040255261e9fae2d47
ryo-a <ryoa.dev@gmail.com>
2020-03-16 14:36:04 +0900

    Jest対応

--- test/test.js
+++ test/test.js
@@ -7,83 +7,88 @@ const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
 
 describe('/login', () => {
-  before(() => {
+  beforeAll(() => {
     passportStub.install(app);
     passportStub.login({ username: 'testuser' });
   });
 
-  after(() => {
+  afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
-
-  it('ログインのためのリンクが含まれる', (done) => {
-    request(app)
+  test('ログインのためのリンクが含まれる', () => {
+    return request(app)
       .get('/login')
       .expect('Content-Type', 'text/html; charset=utf-8')
       .expect(/<a href="\/auth\/github"/)
-      .expect(200, done);
+      .expect(200);
   });
-
-  it('ログイン時はユーザー名が表示される', (done) => {
-    request(app)
+  
+  test('ログイン時はユーザー名が表示される', () => {
+    return request(app)
       .get('/login')
       .expect(/testuser/)
-      .expect(200, done);
+      .expect(200);
   });
 });
 
 describe('/logout', () => {
-  it('/ にリダイレクトされる', (done) => {
-    request(app)
+  test('/ にリダイレクトされる', () => {
+    return request(app)
       .get('/logout')
       .expect('Location', '/')
-      .expect(302, done);
+      .expect(302);
   });
 });
 
 describe('/schedules', () => {
-  before(() => {
+  beforeAll(() => {
     passportStub.install(app);
     passportStub.login({ id: 0, username: 'testuser' });
   });
 
-  after(() => {
+  afterAll(() => {
     passportStub.logout();
     passportStub.uninstall(app);
   });
 
-  it('予定が作成でき、表示される', (done) => {
+  test('予定が作成でき、表示される', (done) => {
     User.upsert({ userId: 0, username: 'testuser' }).then(() => {
       request(app)
-        .post('/schedules')
-        .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
-        .expect('Location', /schedules/)
-        .expect(302)
-        .end((err, res) => {
-          const createdSchedulePath = res.headers.location;
-          request(app)
-            .get(createdSchedulePath)
-            .expect(/テスト予定1/)
-            .expect(/テストメモ1/)
-            .expect(/テストメモ2/)
-            .expect(/テスト候補1/)
-            .expect(/テスト候補2/)
-            .expect(/テスト候補3/)
-            .expect(200)
-            .end(() => {
-              // テストで作成したデータを削除
-              const scheduleId = createdSchedulePath.split('/schedules/')[1];
-              Candidate.findAll({
-                where: { scheduleId: scheduleId }
-              }).then((candidates) => {
-                candidates.forEach((c) => { c.destroy(); });
-                Schedule.findById(scheduleId).then((s) => { s.destroy(); });
+      .post('/schedules')
+      .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+      .expect('Location', /schedules/)
+      .expect(302)
+      .end((err, res) => {
+        const createdSchedulePath = res.headers.location;
+        request(app)
+          .get(createdSchedulePath)
+          .expect(/テスト予定1/)
+          .expect(/テストメモ1/)
+          .expect(/テストメモ2/)
+          .expect(/テスト候補1/)
+          .expect(/テスト候補2/)
+          .expect(/テスト候補3/)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            // テストで作成したデータを削除
+            const scheduleId = createdSchedulePath.split('/schedules/')[1];
+            Candidate.findAll({
+              where: { scheduleId: scheduleId }
+            }).then((candidates) => {
+              const promises = candidates.map((c) => { return c.destroy(); });
+              Promise.all(promises).then(() => {
+                Schedule.findByPk(scheduleId).then((s) => { 
+                  s.destroy().then(() => { 
+                    if (err) return done(err);
+                    done(); 
+                  });
+                });
               });
-              done();
             });
+          });
         });
     });
   });
-
 });
